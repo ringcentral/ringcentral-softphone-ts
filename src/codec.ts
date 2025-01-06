ce6ffaf8eb71265e055bad08878e66b8975a0c0f
Tyler Liu <tyler4long@gmail.com>
2025-01-06 14:00:00 -0800

    Fix a ts compile issue

--- src/codec.ts
+++ src/codec.ts
@@ -4,6 +4,6 @@ const encoder = new Encoder({ channels: 1, sample_rate: 16000 });
 const decoder = new Decoder({ channels: 1, sample_rate: 16000 });
 
 export const opus = {
-  encode: (pcm: Buffer) => encoder.encode(pcm),
-  decode: (opus: Buffer) => decoder.decode(opus),
+  encode: (pcm: Buffer) => Buffer.from(encoder.encode(pcm)),
+  decode: (opus: Buffer) => Buffer.from(decoder.decode(opus)),
 };
