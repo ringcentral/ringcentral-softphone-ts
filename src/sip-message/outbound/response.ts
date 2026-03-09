86296d49ddaa7b9b84745822a76fbfca4cfba20e
Tyler Liu <tyler4long@gmail.com>
2026-03-09 15:21:43 -0700

    Fix response message header case issue

--- src/sip-message/outbound/response.ts
+++ src/sip-message/outbound/response.ts
@@ -11,10 +11,15 @@ class ResponseMessage extends OutboundMessage {
   ) {
     super(undefined, { ...headers }, body);
     this.subject = `SIP/2.0 ${responseCode} ${responseCodes[responseCode]}`;
-    const keys = ["Via", "From", "To", "Call-ID", "CSeq"];
-    for (const key of keys) {
-      if (inboundMessage.headers[key]) {
-        this.headers[key] = inboundMessage.headers[key];
+    const requiredKeys = new Set(["via", "from", "to", "call-id", "cseq"]);
+    const allKeys = Object.keys(inboundMessage.headers).reduce((acc, key) => {
+      acc[key.toLowerCase()] = key;
+      return acc;
+    }, {} as Record<string, string>);
+    for (const key of requiredKeys) {
+      if (allKeys[key]) {
+        const originalKey = allKeys[key];
+        this.headers[originalKey] = inboundMessage.headers[originalKey];
       }
     }
   }
