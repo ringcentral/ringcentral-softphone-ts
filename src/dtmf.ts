a949554062035814fd136d9a528c676ac710cf55
Tyler Liu <tyler4long@gmail.com>
2026-04-21 13:49:59 -0700

    No class with all static members

--- src/dtmf.ts
+++ src/dtmf.ts
@@ -1,42 +1,42 @@
 import { Buffer } from "node:buffer";
 
-class DTMF {
-  public static readonly phoneChars = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "*",
-    "#",
-  ];
-  private static readonly payloads = [
-    0x00060000, 0x000600a0, 0x00060140, 0x00860320, 0x00860320, 0x00860320,
-  ];
+const phoneChars = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "*",
+  "#",
+] as const;
+const payloads = [
+  0x00060000, 0x000600a0, 0x00060140, 0x00860320, 0x00860320, 0x00860320,
+];
 
-  public static charToPayloads = (char: string) => {
-    const index = DTMF.phoneChars.indexOf(char[0]);
+const DTMF = {
+  phoneChars,
+  charToPayloads(char: string) {
+    const index = phoneChars.indexOf(char[0] as (typeof phoneChars)[number]);
     if (index === -1) {
       throw new Error("invalid phone char");
     }
-    return DTMF.payloads.map((payload) => {
+    return payloads.map((payload) => {
       const temp = payload + index * 0x01000000;
       const buffer = Buffer.alloc(4);
       buffer.writeIntBE(temp, 0, 4);
       return buffer;
     });
-  };
-
-  public static payloadToChar = (payload: Buffer) => {
+  },
+  payloadToChar(payload: Buffer) {
     const intBE = payload.readIntBE(0, 4);
     const index = (intBE - 0x00060000) / 0x01000000;
-    return DTMF.phoneChars[index];
-  };
-}
+    return phoneChars[index];
+  },
+};
 
 export default DTMF;
