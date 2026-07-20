ffadfa0937d879995544dd221bffda0c93fa59e5
Tyler Liu <tyler4long@gmail.com>
2026-07-20 13:30:42 -0700

    Start working on 2.0

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/**/*.ts"],
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  cjsDefault: false,
   clean: true,
 });
